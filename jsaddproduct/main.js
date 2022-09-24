3c0742552d1e185648714c04632d723fee98241c
doantruc1989 <ma.quy1987@gmail.com>
2022-09-24 20:02:24 +0700

    baitapjs

---
+++ jsaddproduct/main.js
@@ -0,0 +1,9 @@
+var listProduct = [
+    'dua leo',
+    'ca chua',
+    'rau xanh',
+]
+function myFunction() {
+    var product = prompt("nhap ten san pham", "ca tim");
+    document.getElementById('demo').innerHTML = 'them' + product + 'vao danh sach san pham'
+}
