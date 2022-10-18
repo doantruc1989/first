82fc1fe563374e03185c0355ee84c4c570d2714c
doantruc1989 <113985659+doantruc1989@users.noreply.github.com>
2022-10-18 17:10:48 +0700

    Delete nodejs/login3 directory

--- nodejs/login3/client/public/profile.js
+++
@@ -1,5 +0,0 @@
-
-window.addEventListener("load", () => {
-    var getItem = document.cookie;
-    document.getElementById('name').innerHTML = 'Welcome back, ' + getItem.slice(9,getItem.length);
-})
